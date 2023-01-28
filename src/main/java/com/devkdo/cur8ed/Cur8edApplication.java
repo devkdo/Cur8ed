4c60dd4f09ee06716ee1c2c4fb7edcafcdbb8d93
dekaido <kml5197@outlook.com>
2023-01-28 18:46:30 -0500

    #3 Jumpstart spring project from start.spring.io

---
+++ src/main/java/com/devkdo/cur8ed/Cur8edApplication.java
@@ -0,0 +1,13 @@
+package com.devkdo.cur8ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cur8edApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cur8edApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkdo/cur8ed/Cur8edApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkdo.cur8ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cur8edApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
